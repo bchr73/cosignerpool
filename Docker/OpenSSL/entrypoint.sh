ed9f5e7d9fc951e9805fedeb37bb0accbcd4755b
Karim Boucher <boucherkarim@gmail.com>
2019-10-17 10:10:08 -0400

    Update OpenSSL entrypoint

--- Docker/OpenSSL/entrypoint.sh
+++ Docker/OpenSSL/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -f /etc/certs/fullchain.pem ]
 then
 	echo "no certs found, generating"
-	openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/certs/fullchain.pem -keyout /etc/certs/privkey.pem
+	openssl req -subj '/C=US/ST=Florida/L=Fort Lauderdale/O=Fluid Chains Inc./OU=Fluid Chains Inc./CN=cosigner.exos.to' -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/certs/fullchain.pem -keyout /etc/certs/privkey.pem
 else
 	echo "found certs, attempting renewal"
 	exit 0
