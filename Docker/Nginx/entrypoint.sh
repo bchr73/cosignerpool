7ac9d366f67b947fe8c78a675aa1c3bdb6bf8672
Karim Boucher <boucherkarim@gmail.com>
2019-10-17 10:09:39 -0400

    Update nginx entrypoint

--- Docker/Nginx/entrypoint.sh
+++ Docker/Nginx/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-while [ ! -f /etc/letsencrypt/live/cosigner/fullchain.pem ]
+while [ ! -f /etc/certs/fullchain.pem ]
 do
 	echo "sleeping"
 	sleep 5
 done
 
-echo $NGINX_HOST
+echo starting $NGINX_HOST
 /bin/bash -c "envsubst < /etc/nginx/conf.d/CosignerpoolSSL.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
