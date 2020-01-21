6eb2a15973c14f839a33d5dacf7730e76941bdca
Karim Boucher <boucherkarim@gmail.com>
2020-01-21 14:13:12 -0500

    Run startup script every 7 days

--- Docker/Certbot/entrypoint.sh
+++ Docker/Certbot/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [ ! -f /etc/letsencrypt/live/cosigner/fullchain.pem ]
-then
-	echo "no certs found, attempting acme challenge"
-	certbot certonly --webroot --webroot-path=/var/www/html --email $CONTACT_EMAIL --agree-tos --no-eff-email --force-renewal --cert-name cosigner $CERT_DOMAINS
-else
-	echo "found certs, attempting renewal"
-	certbot renew
-	exit 0
-fi
+while :
+do
+    if [ ! -f /etc/letsencrypt/live/cosigner/fullchain.pem ]
+    then
+        echo "no certs found, attempting acme challenge"
+        certbot certonly --webroot --webroot-path=/var/www/html --email $CONTACT_EMAIL --agree-tos --no-eff-email --force-renewal --cert-name cosigner $CERT_DOMAINS
+    else
+        echo "found certs, attempting renewal"
+        certbot renew
+        exit 0
+    fi
+    sleep 7d
+done
